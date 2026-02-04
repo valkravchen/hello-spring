25216b23955027bbc49338e0704a0568d115dc81
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-04 20:03:18 +0300

    BookNotFoundException

--- src/main/java/com/dobrynya/hellospring/exception/BookNotFoundException.java
+++ src/main/java/com/dobrynya/hellospring/exception/BookNotFoundException.java
@@ -1,7 +1,6 @@
 package com.dobrynya.hellospring.exception;
 
 public class BookNotFoundException extends RuntimeException{
-
     public BookNotFoundException(Long id) {
         super("Книга с id " + id + " не найдена");
     }
