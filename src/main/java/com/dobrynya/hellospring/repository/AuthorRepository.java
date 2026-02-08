3fdef797aaf035dcaee566cfa94da04f0921d4d2
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-08 09:37:34 +0300

    Обновлён класс AuthorRepository

--- src/main/java/com/dobrynya/hellospring/repository/AuthorRepository.java
+++ src/main/java/com/dobrynya/hellospring/repository/AuthorRepository.java
@@ -3,5 +3,8 @@ package com.dobrynya.hellospring.repository;
 import com.dobrynya.hellospring.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findByNameIgnoreCase(String name);
 }
