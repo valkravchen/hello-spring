755aba5af4838724ac3152ed6f493ff54b46224f
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-08 09:38:56 +0300

    Обновлён класс TagRepository

--- src/main/java/com/dobrynya/hellospring/repository/TagRepository.java
+++ src/main/java/com/dobrynya/hellospring/repository/TagRepository.java
@@ -3,5 +3,8 @@ package com.dobrynya.hellospring.repository;
 import com.dobrynya.hellospring.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByNameIgnoreCase(String name);
 }
