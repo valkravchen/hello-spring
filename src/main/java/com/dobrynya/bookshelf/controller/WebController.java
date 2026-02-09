22dcf3752493f4681214bbaca96355bc22f632a9
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-09 11:31:56 +0300

    Обновлены шаблон index.html и класс WebController(добавлены поле и конструктор)

--- src/main/java/com/dobrynya/bookshelf/controller/WebController.java
+++ src/main/java/com/dobrynya/bookshelf/controller/WebController.java
@@ -1,16 +1,26 @@
 package com.dobrynya.bookshelf.controller;
 
+import com.dobrynya.bookshelf.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
+
 @Controller
 public class WebController {
+    private final BookService bookService;
+
+    public WebController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("appName", "BookShelf");
         model.addAttribute("message", "Добро пожаловать в библиотеку!");
+        model.addAttribute("bookCount", bookService.findAll().size());
+        model.addAttribute("currentDate", LocalDate.now());
         return "index";
     }
 }
