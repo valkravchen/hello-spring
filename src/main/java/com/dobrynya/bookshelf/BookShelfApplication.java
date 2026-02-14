955bd70ecfdcc214fb4b1d6c2eba0945f3468645
valkravchen <100293539+valkravchen@users.noreply.github.com>
2026-02-14 10:52:10 +0300

    Количество бинов

--- src/main/java/com/dobrynya/bookshelf/BookShelfApplication.java
+++ src/main/java/com/dobrynya/bookshelf/BookShelfApplication.java
@@ -2,11 +2,14 @@ package com.dobrynya.bookshelf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BookShelfApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookShelfApplication.class, args);
-	}
+    public static void main(String[] args) {
+//		SpringApplication.run(BookShelfApplication.class, args);
+        ApplicationContext context = SpringApplication.run(BookShelfApplication.class);
+        System.out.println("Ответ " + context.getBeanDefinitionCount());
+    }
 }
